7f2dd5ae63330f13100cf56173ef23e1ab3a0f45
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2019-12-04 18:00:26 +0000

    all extensions complete

--- models/artist.rb
+++ models/artist.rb
@@ -57,5 +57,11 @@ class Artist
     return artist
   end
 
+  def update()
+    sql = "UPDATE artists SET name = $1 WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
 
 end
