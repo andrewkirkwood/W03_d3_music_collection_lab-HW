e1e8dc88375aa25057fe53843cebba9641bf8f47
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2019-12-04 15:50:01 +0000

    artist save function written, still to be checked

---
+++ db/console.rb
@@ -0,0 +1,11 @@
+require('pry')
+require_relative('../models/album')
+require_relative('../models/artist')
+
+#add DELETE ALL for both
+
+artist1 = Artist.new({ 'name' => 'Mariah'})
+
+
+binding.pry
+nil
--- models/artist.rb
+++ models/artist.rb
@@ -10,5 +10,17 @@ class Artist
     @name = options['name']
   end
 
+  def save()
+    sql =
+    "
+    INSERT INTO artists (
+      name
+    )
+    VALUE ( $1)
+    RETURNING id;
+    "
+    values = [@name]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
 
 end
