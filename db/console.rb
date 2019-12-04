a4806afb920cd7e2cb412c0e422c233951b25324
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2019-12-04 16:43:35 +0000

    artist list all method passed

--- db/console.rb
+++ db/console.rb
@@ -2,9 +2,9 @@ require('pry')
 require_relative('../models/artist')
 require_relative('../models/album')
 
-
-Artist.delete_all()
 Album.delete_all()
+Artist.delete_all()
+
 
 
 artist1 = Artist.new({ 'name' => 'Mariah'})
